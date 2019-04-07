7b780e745011355ff38596aacf632eca95f4c9d7
MrMcMaloows <greerishere@gmail.com>
2019-04-07 11:47:53 -0400

    added root login if needed

--- cppasswordslin.py
+++ cppasswordslin.py
@@ -1,27 +1,54 @@
 import os
-import sys
 import subprocess
-import re
-        
-users = subprocess.check_output('ls /home',shell=True).decode('utf-8')
+import atexit
+import getpass
 
-userlist=(users.split())
+#os.system('clear')
 
-you = input('Your username: ')
+username = getpass.getuser()
 
-unique = 1
+def rootlogin():
+    os.system('sudo python3 cppasswordslin.py')
 
-index = 0
+def checkroot():
+    if username != 'root':
+        print('In order to run this script you need to be root')
+        while True:
+            exitc = input('Would you like to restart the script as root? [Y/n] ').lower()
+            if exitc=='y':
+                atexit.register(rootlogin)
+                exit()
+            if exitc=='n':
+                break
+            else:
+                continue
+def main():
+    users = subprocess.check_output('ls /home',shell=True).decode('utf-8')
 
-userlist.remove(you)
+    userlist=(users.split())
 
-userlist=sorted(userlist)
+    #os.system('clear')
 
-for word in userlist:
-    password="Cyb3rP4tr10t#" + str(unique)
-    print(word + ": " + password)
-    os.system("echo {}:{} | /usr/sbin/chpasswd".format(word, password))
-    unique=unique+1
+    you = input('Your username: ')
 
-input('Done')
+    unique = 1
 
+    try:
+        userlist.remove(you)
+    except ValueError:
+        print("username entered is invalid! exiting...")
+        exit()
+
+    userlist=sorted(userlist)
+
+    for word in userlist:
+        password="Cyb3rP4tr10t#" + str(unique)
+        print(word + ": " + password)
+        os.system("echo {}:{} | /usr/sbin/chpasswd".format(word, password))
+        unique=unique+1
+
+    input('Done')
+    exit()
+if __name__=='__main__':
+    checkroot()
+    main()
